48f0e5a6fcf74c9e6fbcfe513dd144a2d6c9a0df
Manuel Vicario Pérez <manuel.vicario.perez@gmail.com>
2022-02-19 11:32:32 +0100

    test for get comments done

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+from random import betavariate
 import requests
+from bs4 import BeautifulSoup
+from bs4 import Comment
+
 
 URL = "https://www.knauf.de"
+
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.text,'html.parser')
+comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+for c in comments:
+    print(c)
+    print("===========")
+    c.extract()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
